18b104e14506e7aece8997757411285065333b16
Vargax01 <miguelchico14@gmail.com>
2023-08-21 13:05:12 +0200

    Update setup-django-server.sh
    wkthmltopdf

--- setup-django-server.sh
+++ setup-django-server.sh
@@ -16,3 +16,4 @@ sudo apt-get install python-certbot-apache
 sudo apt-get install libmysqlclient-dev
 sudo apt-get -y install python3-dev default-libmysqlclient-dev build-essential pkg-config
 sudo apt install certbot python3-certbot-apache
+sudo apt install wkhtmltopdf

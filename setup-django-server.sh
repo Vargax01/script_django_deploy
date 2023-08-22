f9e0da4c156c5e754d07a28803d0599ee8c86cef
Vargax01 <miguelchico14@gmail.com>
2023-08-22 11:05:15 +0200

    Update setup-django-server.sh
    a2enmod proxi

--- setup-django-server.sh
+++ setup-django-server.sh
@@ -17,3 +17,9 @@ sudo apt-get install libmysqlclient-dev
 sudo apt-get -y install python3-dev default-libmysqlclient-dev build-essential pkg-config
 sudo apt install certbot python3-certbot-apache
 sudo apt install wkhtmltopdf
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_balancer
+sudo a2enmod lbmethod_byrequests
+
+
